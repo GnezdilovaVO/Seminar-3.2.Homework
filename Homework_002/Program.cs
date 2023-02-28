93170163daf5da5c19fb5932e24cacdec34c2f61
Valentina <valyapomortseva@gmail.com>
2023-02-28 18:29:27 +0700

    Исправила решение задачи 2

--- Homework_002/Program.cs
+++ Homework_002/Program.cs
@@ -1,5 +1,8 @@
 ﻿string ThreeNumbers (int count) {
 string output = String.Empty;
+while (count > 1000) {
+    count = count/10;   
+}
 int a = count/100;
 if (a == 0) {
     output = ("Третей цифры нет");
@@ -7,7 +10,7 @@ if (a == 0) {
 else {
     int b = count%10;
     output = output + b;
-}
+    }
 return output;
 }
 Console.WriteLine("Введите число N: ");
