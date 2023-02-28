f8e201ffebfbab1642b8ca8784686ffecb930bed
Valentina <valyapomortseva@gmail.com>
2023-02-28 17:00:42 +0700

    Задача 2 решена с помощью метода

--- Homework_002/Program.cs
+++ Homework_002/Program.cs
@@ -1,10 +1,19 @@
-﻿Console.WriteLine("Введите число N: ");
-int N = Convert.ToInt32(Console.ReadLine());
-int a = N/100;
+﻿string ThreeNumbers (int count) {
+string output = String.Empty;
+int a = count/100;
 if (a == 0) {
-    Console.WriteLine("Третей цифры нет");
+    output = ("Третей цифры нет");
 }
 else {
-    int b = N%10;
-    Console.WriteLine(b);
+    int b = count%10;
+    output = output + b;
 }
+return output;
+}
+Console.WriteLine("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+string result = ThreeNumbers(N);
+Console.WriteLine(result);
+
+
+
