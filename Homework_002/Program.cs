01b480f87989f6d09b65482227aaae3f92903406
Valentina <valyapomortseva@gmail.com>
2023-02-28 16:41:36 +0700

    Задача 2 решена. Исправлена блок-схема

--- Homework_002/Program.cs
+++ Homework_002/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int a = N/100;
+if (a == 0) {
+    Console.WriteLine("Третей цифры нет");
+}
+else {
+    int b = N%10;
+    Console.WriteLine(b);
+}
